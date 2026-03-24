ff2965a93a8e8765c7a2b8cccd1a7482fbfb207b
Andrew Lunde <andrew.lunde@sap.com>
2026-03-24 10:28:19 -0400

    Update to 2026.3.23

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,3 @@
-export { v_2026_3_22_0_b2 as current } from './v2026.3.22.0.b2'
-export const other = []
+export { v_2026_3_23_0_b0 as current } from './v2026.3.23.0.b0'
+import { v_2026_3_22_0_b2 } from './v2026.3.22.0.b2'
+export const other = [v_2026_3_22_0_b2]
