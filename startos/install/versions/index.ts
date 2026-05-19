cb1f75561026f78f986012f8ede8532f0e0cb295
Andrew Lunde <andrew.lunde@sap.com>
2026-05-19 15:30:03 -0400

    v2026-5-18

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,6 @@
-export { v_2026_5_4_0_b0 as current } from './v2026.5.4.0.b0'
+export { v_2026_5_18_0_b0 as current } from './v2026.5.18.0.b0'
+import { v_2026_5_7_0_b0 } from './v2026.5.7.0.b0'
+import { v_2026_5_4_0_b0 } from './v2026.5.4.0.b0'
 import { v_2026_5_3_0_b0 } from './v2026.5.3.0.b0'
 import { v_2026_4_15_0_b0 } from './v2026.4.15.0.b0'
 import { v_2026_4_14_0_b0 } from './v2026.4.14.0.b0'
@@ -10,6 +12,8 @@ import { v_2026_3_23_0_b0 } from './v2026.3.23.0.b0'
 import { v_2026_3_13_0_b0 } from './v2026.3.13.0.b0'
 import { v_2026_3_12_0_b0 } from './v2026.3.12.0.b0'
 export const other = [
+  v_2026_5_7_0_b0,
+  v_2026_5_4_0_b0,
   v_2026_5_3_0_b0,
   v_2026_4_15_0_b0,
   v_2026_4_14_0_b0,
